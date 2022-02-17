3879e49210ee619b48f85141c56d57e1876a2b1b
Brian Serein <bserein22@gmail.com>
2022-02-17 10:57:16 -0500

    framed out api

---
+++ index.js
@@ -0,0 +1,21 @@
+// to add them do --> npm i firebase-admin express cors <-- do this to add them
+const express = require('express'); //<-- ES5 import 
+const cors = require('cors'); //<-- allows cross origin resource sharing
+const { getTasks } = require('./src/tasks')
+const PORT = process.env.PORT || 3000
+//env is for environmental variable 
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//Routes
+// app.post('/tasks', createTask);
+app.get('/tasks', getTasks);
+// app.patch('/tasks/:taskId', updateTask)
+
+app.listen(PORT, () => {
+    console.log('Listening on PORT: ', PORT)
+});
+
---
+++ src/tasks.js
@@ -0,0 +1,15 @@
+//ES 6 way
+//export function createTask() {}
+
+//ES5 way
+// exports.createTask = (request, response) => {
+
+// }
+exports.getTasks = (request, response) => {
+    response.send("Get Tasks is working.")
+}
+
+// exports.updateTask = (request, response) => {
+
+// }
+
